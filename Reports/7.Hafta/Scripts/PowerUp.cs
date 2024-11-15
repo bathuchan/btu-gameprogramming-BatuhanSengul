03719d1fc18a44c613f92162653581151364497b
Batuhan Şengül <80199404+bathuchan@users.noreply.github.com>
2024-11-15 10:41:39 +0100

    7.week

---
+++ Reports/7.Hafta/Scripts/PowerUp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum PowerUpType 
+    {
+        TripleShotPowerup,
+        SpeedPowerup,//diğer powerupları bu şekilde eklemek daha kolay olacak
+        Option3,
+    }
+    public PowerUpType type;
+    private PlayerMovement pm;
+
+    [Header("Downward Movement")]
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float speedRandomMultiplier = 1f;
+
+
+
+    private void Start()
+    {
+        pm = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        speed = speed + Random.Range(0, speedRandomMultiplier);
+    }
+
+    private void Update()
+    {
+        transform.position = transform.position + new Vector3(0, -speed * Time.deltaTime, 0);
+        CheckPosition();
+    }
+
+    private void CheckPosition()
+    {
+        if (transform.position.y < -(pm.yBorderValue + 1.20f))
+        {
+
+            Destroy(gameObject);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            switch (type) 
+            {
+                case PowerUpType.TripleShotPowerup:
+                    pm.StartTriplePowerup();
+                    break;
+
+                case PowerUpType.SpeedPowerup:
+                    pm.StartSpeedPowerUp();
+                    break;
+
+                default:
+                    Debug.LogWarning("PowerUp type doesnt declared!");
+                    break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
